010e044f46937dfe74e67b956d302d89ad15611a
abhinavd0007 <abhinavd0007@gmail.com>
2026-07-19 13:51:12 +0530

    ollama integrated

---
+++ ai.py
@@ -0,0 +1,16 @@
+import ollama
+
+
+def ask_ai(prompt):
+
+    response = ollama.chat(
+        model="qwen2.5:3b",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response["message"]["content"]
--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from screen import capture_screen
 from ocr import read_text_from_image
-print("Capturing screen...")
+from ai import ask_ai
 
-path = capture_screen()
+def main():
+    print("Capturing screen...")
 
-print("✔ Screenshot saved successfully! \n Location: \n", path)
-text = read_text_from_image(path)
-print(text)
+    path = capture_screen()
+
+    print("✔ Screenshot saved successfully! \n Location: \n", path)
+    text = read_text_from_image(path)
+    print(text)
+
+    if not text.strip():
+        print("\nNo text detected.")
+        return
+
+    print("\nThinking...\n")
+
+    prompt = f"""
+You are a helpful AI tutor.
+
+Explain the following text in simple words.
+
+{text}
+"""
+
+    answer = ask_ai(prompt)
+
+    print("AI Response:")
+    print("-" * 50)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
